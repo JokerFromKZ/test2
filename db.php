81dba56f011d47f33fa4326dee41caa834419678
JokerFromKZ <rus.mur2001@gmail.com>
2020-07-01 00:57:38 +0500

    dfd

--- db.php
+++ db.php
@@ -1,16 +1,24 @@
 <?php
-
 header('Content-type: json/application');
+$user = 'root';
+$password = 'root';
+$db = 'checks';
+$host = 'localhost';
 
-$connect = mysqli_connect('localhost' , 'root', 'root', 'checks');
-
-require 'function.php';
-
-$type = $_GET['q'];
-
-if($type === 'request'){
-
-        requests($connect);
+$dsn = 'mysql:host='. $host .';dbname='.$db; // добавил свой порт к которому подключена база
+$pdo = new PDO($dsn, $user, $password); // подкл. к базе
 
 
+$query = $pdo->prepare('SELECT * FROM `request` ');
+$query->execute();
+
+
+if($_GET['auth_key'] == 'junior_test'){
+    $data = array();
+    while($result =  $query->fetch(PDO::FETCH_ASSOC)){ // оформим каждую строку результата
+        // как ассоциативный массив
+        $data[] = $result; // допишем строку из выборки как новый элемент результирующего массива
+
+    }
+    echo json_encode($data);
 }
